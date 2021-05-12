72690ab2046d5619bec63963e34b6126e56d44fb
Kumar Rohan <kumar.rohan74@gmail.com>
2021-05-12 19:04:02 +0530

    Add files via upload

---
+++ api/addSubCategory.js
@@ -0,0 +1,35 @@
+//header files
+const mongoose = require('mongoose');
+
+//product schema
+const subcategorySchema = new mongoose.Schema({
+    subcategory_name: String,
+    subcategory_image: String,
+    subcategory_status: String,
+    subcategory_priority: Number,
+    subcategory_descrption: String,
+    created_at: {type: Date, default: Date.now }
+});
+
+//create product api
+const SubCategory = mongoose.model('SubCategory', subcategorySchema);
+async function createSubCategory(data)
+{
+    const subcategory = new SubCategory({
+        subcategory_name: data.subcategoryName,
+        subcategory_image: data.subcategoryimage,
+        subcategory_status: data.subcategorystatus,
+        subcategory_priority: data.scpriority,
+        subcategory_descrption: data.subcatdesc
+    });
+    const result = await subcategory.save();
+    console.log(result);
+}
+async function getSubCategory()
+{
+    const subcategory = await SubCategory.find();
+    console.log(subcategory);
+    
+    
+}
+module.exports = { createSubCategory, getSubCategory};
